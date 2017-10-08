7d46619f12f9d17be33c026004c3f1dd21628e5a
kevinvud <kevinvud@gmail.com>
2017-10-07 18:51:19 -0700

    Setup SWReveal, add gesture recognizer, and spacing between rear and front VC

---
+++ SlakChat/Controller/ChatVC.swift
@@ -0,0 +1,28 @@
+//
+//  ChatVC.swift
+//  SlakChat
+//
+//  Created by PoGo on 10/7/17.
+//  Copyright © 2017 PoGo. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    
+    @IBOutlet weak var menuBtn: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+        
+    }
+
+
+
+}
