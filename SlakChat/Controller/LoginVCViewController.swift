a40de7c312777d866e088ba491ec33a2371d1f53
kevinvud <kevinvud@gmail.com>
2017-10-07 21:17:26 -0700

    Add loginVC, set up UI

---
+++ SlakChat/Controller/LoginVCViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginVCViewController.swift
+//  SlakChat
+//
+//  Created by PoGo on 10/7/17.
+//  Copyright © 2017 PoGo. All rights reserved.
+//
+
+import UIKit
+
+class LoginVCViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+
+}
---
+++ SlakChat/Utillities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SlakChat
+//
+//  Created by PoGo on 10/7/17.
+//  Copyright © 2017 PoGo. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
