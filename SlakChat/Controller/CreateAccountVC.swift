6c09186c1b38e44e6ef3d9c1fea9e8afd83dd202
kevinvud <kevinvud@gmail.com>
2017-10-08 22:09:57 -0700

    Pass user registration info to server

--- SlakChat/Controller/CreateAccountVC.swift
+++ SlakChat/Controller/CreateAccountVC.swift
@@ -18,6 +18,8 @@ class CreateAccountVC: UIViewController {
     
     @IBOutlet weak var userImg: UIImageView!
     
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5,0.5,0.5,1]"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,14 +34,23 @@ class CreateAccountVC: UIViewController {
 
     @IBAction func createAccntPressed(_ sender: Any) {
         
+        guard let name = userNameTxt.text , userNameTxt.text != "" else{ return }
         guard let email = emailTxt.text , emailTxt.text != "" else{ return }
         guard let pass = passTxt.text, passTxt.text != "" else{return}
-        AutheService.instance.registerUser(email: email, password: pass) { (success) in
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
             if success{
-                print("regustered user!")
-                AutheService.instance.loginUser(email: email, password: pass, completion: { (success) in
+                print("registered user!")
+                AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
                     if success{
-                        print("logged in user~", AutheService.instance.authToken)
+                        print("logged in user~", AuthService.instance.authToken)
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success{
+                                print("Success")
+                                print(UserDataService.instance.name, UserDataService.instance.avatarName)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
                     }
                 })
             }
--- SlakChat/Services/AuthService.swift
+++ SlakChat/Services/AuthService.swift
@@ -10,9 +10,9 @@ import Foundation
 import Alamofire
 import SwiftyJSON
 
-class AutheService{
+class AuthService{
     
-    static let instance = AutheService()
+    static let instance = AuthService()
     
     let defaults = UserDefaults.standard
     
@@ -104,7 +104,42 @@ class AutheService{
     
     
     }
-    
+    func createUser(name: String, email: String, avatarName: String, avatarColor: String, completion: @escaping CompletionHandler){
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let body: [String: Any] = [
+            "name": name,
+            "email": lowerCaseEmail,
+            "avatarName": avatarName,
+            "avatarColor": avatarColor
+        ]
+        
+        let header = [
+            "Authorization":"Bearer \(AuthService.instance.authToken)",
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        Alamofire.request(URL_USER_ADD, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseJSON { (response) in
+            if response.result.error == nil{
+                guard let data = response.data else{return}
+                let json = JSON(data: data)
+                let id = json ["_id"].stringValue
+                let color = json ["avatarColor"].stringValue
+                let avatarName = json ["avatarName"].stringValue
+                let email = json ["email"].stringValue
+                let name = json ["name"].stringValue
+                
+                UserDataService.instance.setUserData(id: id, color: color, avatarName: avatarName, email: email, name: name)
+                completion(true)
+            }else{
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+        
+        
+    }
     
     
     
