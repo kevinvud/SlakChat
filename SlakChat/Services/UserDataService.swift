bb041de4054413cdc4082316d30d1b72b0e8d0bb
kevinvud <kevinvud@gmail.com>
2017-10-09 03:18:09 -0700

    Set up Notification Center, scan avatar color string and convert to CGFloat and display in ChanelVC

--- SlakChat/Services/UserDataService.swift
+++ SlakChat/Services/UserDataService.swift
@@ -31,4 +31,36 @@ class UserDataService{
         self.avatarName = avatarName
     }
     
+    func returnUIColor(components: String) -> UIColor{
+        let scanner = Scanner(string: components)
+        let skipped = CharacterSet(charactersIn: "[], ")
+        let comma = CharacterSet(charactersIn: ",")
+        scanner.charactersToBeSkipped = skipped
+        
+        var r, g, b, a : NSString?
+        scanner.scanUpToCharacters(from: comma, into: &r)
+        scanner.scanUpToCharacters(from: comma, into: &g)
+        scanner.scanUpToCharacters(from: comma, into: &b)
+        scanner.scanUpToCharacters(from: comma, into: &a)
+        
+        let defautColor = UIColor.lightGray
+        
+        guard let rUnwrapped = r else {return defautColor}
+        guard let gUnwrapped = g else {return defautColor}
+        guard let bUnwrapped = b else {return defautColor}
+        guard let aUnwrapped = a else {return defautColor}
+        
+        //have to convert to double first before convert to CGFloat
+        let rfloat = CGFloat(rUnwrapped.doubleValue)
+        let gfloat = CGFloat(gUnwrapped.doubleValue)
+        let bfloat = CGFloat(bUnwrapped.doubleValue)
+        let afloat = CGFloat(aUnwrapped.doubleValue)
+        
+        let newUIColor = UIColor(red: rfloat, green: gfloat, blue: bfloat, alpha: afloat)
+        
+        return newUIColor
+        
+        
+        
+    }
 }
