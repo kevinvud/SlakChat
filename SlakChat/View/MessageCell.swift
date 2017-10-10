93d4dd54df363aa4437a3572b3a4f91b82e34680
kevinvud <kevinvud@gmail.com>
2017-10-10 04:04:30 -0700

    Display date and time in chat

--- SlakChat/View/MessageCell.swift
+++ SlakChat/View/MessageCell.swift
@@ -32,6 +32,27 @@ class MessageCell: UITableViewCell {
         userImage.image = UIImage(named: message.userAvatar)
         userImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
         
+        //2017-07-13T31:11:11Z
+        
+        guard var isoDate = message.timeStamp else {return}
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = String(isoDate.prefix(upTo: end))
+
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+
+        let newFormatter = DateFormatter()
+        newFormatter.dateStyle = .long
+        newFormatter.timeStyle = .short
+        newFormatter.doesRelativeDateFormatting = true
+        //newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate{
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStamp.text = finalDate
+            
+        }
+        
     }
 
 }
